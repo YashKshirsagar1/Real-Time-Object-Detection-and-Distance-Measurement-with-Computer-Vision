35c22884b0307a40906e3b5f8b3b993d757cbac5
YashKshirsagar1 <ykshirsagar@eastsideprep.org>
2024-06-14 21:36:44 -0700

    changes

--- objectdetector.py
+++ objectdetector.py
@@ -30,23 +30,23 @@ fonts2 = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
 fonts3 = cv2.FONT_HERSHEY_COMPLEX_SMALL
 fonts4 = cv2.FONT_HERSHEY_TRIPLEX
 # Camera Object
-cap = cv2.VideoCapture(0)  # Number According to Camera
-face_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(1)  # Number According to Camera
+face_model = cv2.CascadeClassifier('C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\haarcascade_frontalface_default.xml')
 Distance_level = 0
 classNames = []
-with open('coco.names','r') as f:
+with open('C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\coco.names','r') as f:
     classNames = f.read().splitlines()
 print(classNames)
 Colors = np.random.uniform(0, 255, size=(len(classNames), 3))
 
-weightsPath = "frozen_inference_graph.pb"
-configPath = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\frozen_inference_graph.pb"
+configPath = "C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output21.mp4', fourcc, 30.0, (640, 480))
+out = cv2.VideoWriter('C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\output21.mp4', fourcc, 30.0, (640, 480))
 
-face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_detector = cv2.CascadeClassifier("C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\haarcascade_frontalface_default.xml")
 
 net = cv2.dnn_DetectionModel(weightsPath,configPath)
 net.setInputSize(320,320)
@@ -146,7 +146,7 @@ def face_data(image, CallOut, Distance_level):
 
 
 # reading reference image from directory
-ref_image = cv2.imread("lena.png")
+ref_image = cv2.imread("C:\\BlindApplication\\Real-Time-Object-Detection-and-Distance-Measurement-with-Computer-Vision\\lena.png")
 
 ref_image_face_width, _, _, _ = face_data(ref_image, False, Distance_level)
 Focal_length_found = FocalLength(
